a1d7f30957e1309077b33d3dc7692fe051a34938
Gregory4321 <greg-lewis@hotmail.co.uk>
2021-01-26 14:29:06 +0000

    Character count and Password match added

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
     $(".sidenav").sidenav();
     $('.collapsible').collapsible();
+    $('input#username, input#password, input#confirm-password').characterCounter();
   });
+
+/* 
+Passwords don't match
+code found and taken from https://codepen.io/diegoleme/pen/surIK
+*/
+
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm-password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
