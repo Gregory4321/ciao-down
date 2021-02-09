6e928b3960e5759dbe5cf7609d93e454567b143c
Gregory4321 <greg-lewis@hotmail.co.uk>
2021-02-09 13:30:06 +0000

    Added Back button to enhance UX

--- static/js/script.js
+++ static/js/script.js
@@ -84,3 +84,7 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function backButton() {
+  window.history.back();
+}
